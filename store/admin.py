5f484d663630cbac0b3a6cb136944275f1787fea
adaltair <adaltair03@gmail.com>
2022-03-07 16:40:00 +0600

    models

---
+++ store/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name', )}
+    
