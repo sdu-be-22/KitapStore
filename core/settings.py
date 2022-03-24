74b673718b374e8f97fd7f9b68030af1885d221d
adaltair <adaltair03@gmail.com>
2022-03-25 01:23:26 +0600

    basket finished

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'store.views.categories',
-                
+                'basket.context_processors.basket',
             ],
         },
     },
